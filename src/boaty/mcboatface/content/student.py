91032788ee104a5511dce2740f42f6c6c1d470c4
Hilary Mark Nelson <hilary.nelson@gmail.com>
2023-02-23 15:59:22 -0500

    boaty.mcboatface addon and browser views

--- src/boaty/mcboatface/content/student.py
+++ src/boaty/mcboatface/content/student.py
@@ -1,22 +1,68 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
+from plone.app.textfield import RichText
 # from plone.autoform import directives
 from plone.dexterity.content import Item
 
-# from plone.namedfile import field as namedfile
+from plone.namedfile import field as namedfile
 from plone.supermodel import model
 
-# from plone.supermodel.directives import fieldset
+from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from zope.interface import implementer
+from zope.interface import invariant, Invalid
 
+from boaty.mcboatface import _
 
-# from boaty.mcboatface import _
+vocab_gender = SimpleVocabulary([
+    SimpleTerm(value='--NOVALUE--', title=_(u'Please Select')),
+    SimpleTerm(value='male', title=_(u'Male')),
+    SimpleTerm(value='female', title=_(u'Female')),
+    SimpleTerm(value='unspecified', title=_(u'Unspecified')),
+])
 
 
 class IStudent(model.Schema):
     """Marker interface and Dexterity Python Schema for Student"""
+
+    studentName = schema.TextLine(
+        title=_(u'Name of student'),
+        required=True
+    )
+
+    age = schema.TextLine(
+        title=_(u'Age of student'),
+        required=True
+    )
+
+    gender = schema.Choice(
+        title=_(u'Gender'),
+        vocabulary=vocab_gender,
+        required=False
+    )
+
+    bio = RichText(
+        title=_(u'Biography'),
+        required=False
+    )
+
+    studentPhoto = namedfile.NamedBlobImage(
+        title=_(u'Student photo'),
+        required=False,
+    )
+
+    fieldset('Images', fields=['studentPhoto'])
+    fieldset('Bio', fields=['bio'])
+
+    @invariant
+    def validateNumber(data):
+        if data.age:
+            if not str(data.age).isdigit():
+                raise Invalid(_(u'Age of student must be a number.'))
+            else:
+                if int(data.age) > 110:
+                    raise Invalid(_(u'%s?! Are you kidding?!' % data.age))
 
     # If you want, you can load a xml model created TTW here
     # and customize it in Python:
